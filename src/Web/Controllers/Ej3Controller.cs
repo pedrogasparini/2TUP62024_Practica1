b289c8269c02ca71b4d467112042fb66baa946ea
Franco <cfranco3102@gmail.com>
2024-08-11 15:41:15 -0300

    Ej3 resuelto

---
+++ src/Web/Controllers/Ej3Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Ej3controller : ControllerBase
+{
+
+    [HttpGet]
+    public string Get(string nombre, int edad)
+    {
+
+
+        return $"Hola {nombre}, tienes {edad} años";
+    }
+}
