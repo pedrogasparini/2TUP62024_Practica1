c18c978311f2061359ef3ad8c2c70e1763ecb89b
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-09-17 17:49:50 -0300

    Ej1Linq done

---
+++ src/Web/Controllers/Ej1LinqController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1LinqController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<int>> Get([FromQuery]List<int> list)
+        {
+            List<int> list1 = list;
+
+            if (list == null)
+            {
+                return BadRequest("Lista vacia");
+            }
+
+            var l = list.Where(x => x > 30 && x < 100).ToList();
+            return Ok(l);
+
+
+        }
+    }
+}
