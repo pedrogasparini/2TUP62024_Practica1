fe51ad53a583ff157ecab82aefdfd33a3ddf8619
pedrogasparini <pedrogasparini99@gmail.com>
2024-09-17 14:38:09 -0300

    Ejercicio 10 resuelto

---
+++ src/Web/Controllers/Ej10Controller.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej10Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<int>> Get(int inicio, int fin)
+        {
+            if (fin - inicio >= 200)
+            {
+                return BadRequest("La diferencia maxima entre INICIO y FIN es 200");
+            }
+
+            List<int> pares = [];
+            List<int> impares = [];
+
+            for (int n = inicio; n <= fin; n++)
+            {
+                if (n % 2 == 0)
+                {
+                    pares.Add(n);
+
+                } else
+                {
+                    impares.Add(n);
+                }
+            }
+            return Ok(new{ pares, impares});
+
+        }
+    }
+}
