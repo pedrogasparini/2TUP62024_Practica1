09ccd8a9839b6badb9ef89fc9aa5aa0ffb5c80e3
pedrogasparini <pedrogasparini99@gmail.com>
2024-09-17 14:24:14 -0300

    Ej9 resuelto

---
+++ src/Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej9Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<int>> Get(int inicio, int fin)
+        {
+            if (fin - inicio >= 200)
+            {
+                return BadRequest("La diferencia maxima entre INICIO y FIN es 200");
+            } 
+
+            List<int> nums = [];
+
+            for (int n = inicio; n <= fin; n++)
+            {
+                if (n % 2 == 0)
+                {
+                    nums.Add(n);
+                }
+            }
+
+            return Ok(nums);
+        }
+    }
+}
