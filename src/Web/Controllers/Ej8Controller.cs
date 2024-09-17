36a136965ab1cadd5b7f1eadb513b75c62a702dc
pedrogasparini <pedrogasparini99@gmail.com>
2024-09-17 14:15:54 -0300

    Ej8 resuelto

---
+++ src/Web/Controllers/Ej8Controller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+
+[Route("api/[controller]")]
+[ApiController]
+    public class Ej8Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<int>> Get(int inicio, int fin)
+        {
+            if (fin - inicio >= 200) 
+            {
+                return BadRequest("La diferencia maxima entre INICIO y FIN es 200");
+            }
+
+            List<int> nums = [];
+
+            for (int n = inicio; n <= fin; n++) 
+            { 
+                nums.Add(n);
+            }
+
+            return Ok(nums);
+        }
+    }
+}
