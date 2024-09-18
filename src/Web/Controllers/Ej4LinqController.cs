805c31e95b76184f0a5417cb03f050027efc0308
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-09-18 17:07:44 -0300

    Ej4Linq resuelto

---
+++ src/Web/Controllers/Ej4LinqController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4LinqController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<int>> Get([FromQuery] List<int> numbers)
+        {
+
+            var n = numbers.OrderByDescending(n => n)
+                         .Take(5)
+                         .ToList();
+
+
+
+            return Ok(n);
+
+        }
+    }
+}
