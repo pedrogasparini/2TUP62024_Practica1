9806f1d6518892fc7bb1abc0e7ecf46d4eb1bfe6
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-09-18 11:32:46 -0300

    Ej2Linq resuelto

---
+++ src/Web/Controllers/Ej2LinqController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2LinqController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> Get([FromQuery] List<string> words )
+        {
+
+            foreach (var word in words)
+            {
+                if (word == null)
+                {
+                    return BadRequest("Palabra vacia/invalida.");
+                }
+            }
+
+
+            var w = words.Where(w=> w.Length>5)
+                         .Select(w=>w.ToUpper())
+                         .ToList();
+
+            
+
+            return Ok(w);
+            
+        }
+    }
+}
