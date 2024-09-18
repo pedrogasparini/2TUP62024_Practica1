2cd25aa5032f20827f5c9a5f6650cecc1400834d
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-09-18 17:53:13 -0300

    Ej6Linq resuelto

---
+++ src/Web/Controllers/Ej6LinqController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers 
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej6LinqController : ControllerBase
+    {
+        [HttpGet]
+
+        public ActionResult<List<string>> Get([FromQuery] List<string> words) 
+        {
+            foreach (var word in words)
+            {
+                if (word == null)
+                {
+                    return BadRequest("Palabra invalida/vacia.");
+                }
+            }
+
+            var w = words.Select(w => w.Replace( "ia", "*")).ToList();
+
+            return Ok(w);
+        }
+    }
+}
+
