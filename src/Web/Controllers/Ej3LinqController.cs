4a8ce3d33a0aa81b46f271d8b19917a4163d7069
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-09-18 12:06:01 -0300

    Ej3 linq resuelto

---
+++ src/Web/Controllers/Ej3LinqController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej3LinqController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> Get([FromQuery] List<string> words)
+        {
+
+            foreach (var word in words)
+            {
+                if (word == null)
+                {
+                    return BadRequest("Palabra vacia/invalida.");
+                }
+            }
+
+
+            var w = words.Where(w => w.StartsWith("b") && w.EndsWith("r"))
+                         .Select(w => w.ToUpper())
+                         .ToList();
+
+
+
+            return Ok(w);
+
+        }
+    }
+}
