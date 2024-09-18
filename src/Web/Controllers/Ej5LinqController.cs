cae76158d296fb1fa49b3cfb28d95f406dc999a3
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2024-09-18 17:21:02 -0300

    Ej5Linq resuelto

---
+++ src/Web/Controllers/Ej5LinqController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej5LinqController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> Get([FromQuery] List<int> numbers)
+        {
+
+            var n = numbers.Where(n => n > 20).ToList();
+
+            var result = n
+                .Select(n => $"Numero: {n} - Cuadrado: {n * n}")
+                .ToList();
+
+            return Ok(result);
+        }
+    }
+}
